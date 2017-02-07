71649c7a659226938f28781e00b8e19d1de24e07
HoratiuC <horatiucovrig@gmail.com>
2017-02-07 16:34:06 -0700

    Update PrimeFactors.py

--- PrimeFactors.py
+++ PrimeFactors.py
@@ -15,10 +15,9 @@ n=int(input("Enter a number:"))
 for i in range (2,int(n/2+1)):
     if n%i == 0:
         n = n/i
-        if isprime(n):
-            while n>1:
+        print (n)
+        if isprime(n) and n>1:
                 print ("Biggest factor:",n)
-        if n==1:
-            break
+                break
     
 
